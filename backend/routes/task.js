ec1eb102f6491b5d722edaf13a8cf6c9e4ce447b
Yashmeet-Swami <154438641+Yashmeet-Swami@users.noreply.github.com>
2025-07-22 10:07:03 +0530

    ✅ Complete task management functionality

--- backend/routes/task.js
+++ backend/routes/task.js
@@ -1,7 +1,22 @@
 import express from "express";
 import { validateRequest } from "zod-express-middleware";
 import { taskSchema } from "../libs/validate-schema.js";
-import { createTask } from "../controllers/task.js";
+import {
+  createTask,
+  getTaskById,
+  updateTaskTitle,
+  updateTaskDescription,
+  updateTaskStatus,
+  updateTaskAssignees,
+  updateTaskPriority,
+  addSubTask,
+  updateSubTask,
+  getActivityByResourceId,
+  getCommentsByTaskId,
+  addComment,
+  archivedTask,
+  watchTask,
+} from "../controllers/task.js";
 import authMiddleware from "../middleware/auth-middleware.js";
 import { z } from "zod";
 
@@ -18,5 +33,134 @@ router.post(
   }),
   createTask
 );
+
+router.post(
+  "/:taskId/add-subtask",
+  authMiddleware,
+  validateRequest({
+    params: z.object({ taskId: z.string() }),
+    body: z.object({ title: z.string() }),
+  }),
+  addSubTask
+);
+
+router.post(
+  "/:taskId/add-comment",
+  authMiddleware,
+  validateRequest({
+    params: z.object({ taskId: z.string() }),
+    body: z.object({ text: z.string() }),
+  }),
+  addComment
+);
+
+router.post(
+  "/:taskId/watch",
+  authMiddleware,
+  validateRequest({
+    params: z.object({ taskId: z.string() }),
+  }),
+  watchTask
+);
+
+router.post(
+  "/:taskId/archived",
+  authMiddleware,
+  validateRequest({
+    params: z.object({ taskId: z.string() }),
+  }),
+  archivedTask
+);
+
+router.get(
+  "/:taskId",
+  authMiddleware,
+  validateRequest({
+    params: z.object({
+      taskId: z.string(),
+    }),
+  }),
+  getTaskById
+)
+
+router.get(
+  "/:resourceId/activity",
+  authMiddleware,
+  validateRequest({
+    params: z.object({ resourceId: z.string() }),
+  }),
+  getActivityByResourceId
+);
+
+router.get(
+  "/:taskId/comments",
+  authMiddleware,
+  validateRequest({
+    params: z.object({ taskId: z.string() }),
+  }),
+  getCommentsByTaskId
+);
+
+router.put(
+  "/:taskId/update-subtask/:subTaskId",
+  authMiddleware,
+  validateRequest({
+    params: z.object({ taskId: z.string(), subTaskId: z.string() }),
+    body: z.object({ completed: z.boolean() }),
+  }),
+  updateSubTask
+);
+
+router.put(
+  "/:taskId/title",
+  authMiddleware,
+  validateRequest({
+    params: z.object({ taskId: z.string() }),
+    body: z.object({ title: z.string() }),
+  }),
+  updateTaskTitle
+)
+
+router.put(
+  "/:taskId/description",
+  authMiddleware,
+  validateRequest({
+    params: z.object({ taskId: z.string() }),
+    body: z.object({ description: z.string() }),
+  }),
+  updateTaskDescription
+)
+
+router.put(
+  "/:taskId/status",
+  authMiddleware,
+  validateRequest({
+    params: z.object({ taskId: z.string() }),
+    body: z.object({ status: z.string() }),
+  }),
+  updateTaskStatus
+)
+
+router.put(
+  "/:taskId/assignees",
+  authMiddleware,
+  validateRequest({
+    params: z.object({ taskId: z.string() }),
+    body: z.object({ assignees: z.array(z.string()) }),
+  }),
+  updateTaskAssignees
+)
+
+router.put(
+  "/:taskId/priority",
+  authMiddleware,
+  validateRequest({
+    params: z.object({ taskId: z.string() }),
+    body: z.object({ priority: z.string() }),
+  }),
+  updateTaskPriority
+);
+
+
 
 export default router;
--- frontend/app/routes.ts
+++ frontend/app/routes.ts
@@ -13,12 +13,18 @@ export default [
     layout("routes/dashboard/dashboard-layout.tsx", [
         route("dashboard", "routes/dashboard/index.tsx"),
         route("workspaces", "routes/dashboard/workspaces/index.tsx"),
-        route("workspaces/:workspaceId", "routes/dashboard/workspaces/workspace-details.tsx"),
-
-
+        route(
+            "workspaces/:workspaceId",
+            "routes/dashboard/workspaces/workspace-details.tsx"
+        ),
         route(
             "workspaces/:workspaceId/projects/:projectId",
             "routes/dashboard/project/project-details.tsx"
         ),
+         route(
+            "workspaces/:workspaceId/projects/:projectId/tasks/:taskId",
+            "routes/dashboard/task/task-details.tsx"
+        ),
+
     ]),
 ] satisfies RouteConfig;
